08a9cec6aa63d6597b7069b442a0da1aec1e1ffc
Sebastien Vallet <sebastien.vallet89@gmail.com>
2024-08-03 10:27:41 +0200

    initial commit

---
+++ src/main/java/com/trodix/demo/sslbundlemanage/SslBundleManageApplication.java
@@ -0,0 +1,13 @@
+package com.trodix.demo.sslbundlemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslBundleManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslBundleManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trodix/demo/sslbundlemanage/SslBundleManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trodix.demo.sslbundlemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslBundleManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
